01feee1d4ed92346679c158f7b431bebc1a2d8ec
Aaric Pittman <aaricpittman@gmail.com>
2017-02-04 20:51:23 -0600

    lesson 7 exercise

---
+++ 07-enum-and-stream/exercise.exs
@@ -0,0 +1,27 @@
+defmodule Lesson7 do
+  def months do
+    Stream.cycle(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+  end
+
+  def n_months(n) do
+    months()
+    |> Enum.take(n)
+  end
+
+  def get_ds do
+    get_words_starting_with("D")
+    |> Enum.sort(&is_longer/2)
+  end
+
+  def get_words_starting_with(start) do
+    get_words()
+    |> Stream.filter(&String.starts_with?(&1, String.capitalize(start)))
+  end
+
+  def get_words do
+    File.stream!("sample.txt", [], :line)
+    |> Stream.map(&String.capitalize/1)
+  end
+
+  def is_longer(x, y), do: (String.length(x) > String.length(y))
+end
